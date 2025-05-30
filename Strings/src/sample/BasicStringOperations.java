488f787413803373792eb386d5cbad5812942729
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-30 20:49:59 +0530

    Update BasicStringOperations.java

--- Strings/src/sample/BasicStringOperations.java
+++ Strings/src/sample/BasicStringOperations.java
@@ -23,8 +23,44 @@ public class BasicStringOperations {
 		System.out.println(str.isEmpty()); // false
 		System.out.println(str.length());// 12
 		System.out.println(str.replace("N","T")); // KodTest Tech
+
+		// Examples
+		String firstname = "Hima";
+		String lastname = "Bindu";
+		String combine = firstname.concat(" ").concat(lastname);
+		System.out.println(combine);
+		
+		int length = combine.length();
+		System.out.println("length: " + length);
+		
+		char initial1 = firstname.charAt(0);
+		char initial2 = lastname.charAt(0);
+		System.out.println("Initials are: " + initial1 + initial2);
+		
+		String sub = combine.substring(0,6);
+		System.out.println("Substring(startIndex,endIndex(exclusive)): " + sub);
+
+		String actual = "bindu123";
+		String duplicate = "bindu123";
+		if(actual.equals(duplicate)) { // check the values
+			System.out.println("They are equals the content of 2 strings");
+		} else {
+			System.out.println("They are not equal");
+		}
 		
 		
+		if(actual==duplicate) {  //check memory referencecs
+			System.out.println("They are referencing same object.");
+		} else {
+			System.out.println("They are not equal");
+		}
+		
+		System.out.println("Uppercase: " + firstname.toUpperCase());
+		System.out.println("Lowercase: " + firstname.toLowerCase());
+		
+		String word = "   bindu123   ";
+		String trimmedWord = word.trim(); // remove leading and trailing spaces
+		System.out.println("After trim(): " + trimmedWord);
 	}
 
 }
