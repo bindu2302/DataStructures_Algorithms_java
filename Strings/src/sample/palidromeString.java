58ede4f71092bfdf9a3854d592edac6eca33d93f
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-31 20:43:46 +0530

    Update palidromeString.java

--- Strings/src/sample/palidromeString.java
+++ Strings/src/sample/palidromeString.java
@@ -8,17 +8,30 @@ public class palidromeString {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a string: ");
 		
-		String str = scan.next();
-		char[] arr = str.toCharArray();
-		char[] revArr = new char[arr.length];
+//		// One method using Stringbuilder
+//		String str = scan.nextLine();
+//		
+//		String reversed = new StringBuilder(str).reverse().toString();
+//		
+//		if(str.equals(reversed)) {
+//			System.out.println("The string is palindrome");
+//		} else {
+//			System.out.println("The string is not palindrome");
+//		}
+
+
+		// Without using built-in function
+		String str = scan.nextLine();
+		char[] charArr = str.toCharArray();
+		char[] revArr = new char[charArr.length];
 		
 		int j = revArr.length-1;
-		for(int i=0; i<=arr.length-1; i++) {
-			revArr[i] = arr[j];
+		for(int i=0; i<=charArr.length-1; i++) {
+			revArr[i] = charArr[j];
 			j--;
 		}
+		
 		String revStr = new String(revArr);
-		System.out.println("The reversed string is: " + revStr);
 		
 		if(str.equals(revStr)) {
 			System.out.println("The string is palindrome");
