52ac8e3ceed0b2f3413bfe0e45144b4b155045bf
HimaBindu Nara <naarahimabindu@gmail.com>
2025-06-16 12:29:14 +0530

    Update StringRotationWithDirection.java

--- Strings/src/sample/StringRotationWithDirection.java
+++ Strings/src/sample/StringRotationWithDirection.java
@@ -15,14 +15,37 @@ public class StringRotationWithDirection {
 		String direction = scan.next().toLowerCase();
 		
 		if(direction.equals("right")) {
-			String s1 = str.substring(str.length()-pos);
-			String s2 = str.substring(0,str.length()-pos);
-			System.out.println(s1+s2);
-			
+			String str1 = str.substring(str.length()-pos);
+			String str2 = str.substring(0,str.length()-pos);
+			System.out.println("Rotated String: " + (str1+str2));
 		} else {
-			String s1 = str.substring(pos);
-			String s2 = str.substring(0,pos);
-			System.out.println(s1+s2);
-		}	
+			String str1 = str.substring(pos);
+			String str2 = str.substring(0,pos);
+			System.out.println("Rotated String: " + (str1+str2));
+		}
 	}
 }
+
+//Enter a string: 
+//HelloWorld
+//Enter no.of positions: 
+//3
+//Enter a direction: 
+//left
+//Rotated String: loWorldHel
+
+//Enter a string: 
+//HelloWorld
+//Enter no.of positions: 
+//2
+//Enter a direction: 
+//right
+//Rotated String: ldHelloWor
+
+//Enter a string: 
+//JavaProgramming
+//Enter no.of positions: 
+//5
+//Enter a direction: 
+//left
+//Rotated String: rogrammingJavaP
